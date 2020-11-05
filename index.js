8ec4ddb6df84f3625c82622863a070942bbe15f7
Angus Kellett <anguskellett@hotmail.com>
2020-11-05 21:16:32 +0000

    require all functions and exports all as object

---
+++ index.js
@@ -0,0 +1,10 @@
+const pitch = require('./pitch');
+const advanceRunners = require('./advanceRunners');
+const updateTotalRunners = require('./updateTotalRunners');
+
+module.exports = {
+  pitch,
+  advanceRunners,
+  updateTotalRunners
+};
+
