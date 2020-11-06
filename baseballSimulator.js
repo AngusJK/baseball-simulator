fdb697cdc74c7314648cb9861b7894464f2e1213
Angus Kellett <anguskellett@hotmail.com>
2020-11-06 23:21:31 +0000

    creates teams and exports all variables

--- baseballSimulator.js
+++ baseballSimulator.js
@@ -1,37 +1,45 @@
-const pitch = require('./index');
-
+const pitch = require('./pitch');
 //const args = process.argv.slice(2);
-let homeTeam = "Dodgers";
-let visitingTeam = "Rays";
-let homeTeamRuns = 0;
-let visitingTeamRuns = 0;
-let runs = 0;
-let teamAtBat = "";
-let batter = 0;
-let homeBatter = 1;
-let visitorBatter = 1;
-let outs = 0;
-let inning = [1, 0];
-let half = "";
-let onFirst = 0;
-let onSecond = 0;
-let onThird = 0;
-let totalRunners = 0;
-// let score = `Score: ${visitingTeam} ${visitingTeamRuns}, ${homeTeam} ${homeTeamRuns}`;
+const dodgers = ["Betts", "Seager", "Turner", "Muncy", "Smith", "Bellinger", "Pollock", "Hernandez", "Taylor"];
+const rays = ["Meadows", "Lowe", "Arozarena", "Choi", "Margot", "Wendell", "Kiermaier", "Zunino", "Adames"];
 
-const team = {
-  name: ""
-};
-
-const player = {
-  firstName: "",
-  lastName: "",
-  fullName: this.firstName + this.lastName,
-  position: "",
-  uniformNumber: "",
-};
+var homeTeam = dodgers;
+var visitingTeam = rays;
+var homeTeamRuns = 0;
+var visitingTeamRuns = 0;
+var runs = 0;
+var teamAtBat = "";
+var batter = 0;
+var homeBatter = 1;
+var visitorBatter = 1;
+var outs = 0;
+var inning = [1, 0];
+var half = "";
+var onFirst = 0;
+var onSecond = 0;
+var onThird = 0;
+var totalRunners = 0;
+// var score = `Score: ${visitingTeam} ${visitingTeamRuns}, ${homeTeam} ${homeTeamRuns}`;
 
 pitch();
+
+module.exports = homeTeam;
+module.exports = visitingTeam;
+module.exports = homeTeamRuns;
+module.exports = visitingTeamRuns;
+module.exports = runs;
+module.exports = teamAtBat;
+module.exports = batter;
+module.exports = homeBatter;
+module.exports = visitorBatter;
+module.exports = outs;
+module.exports = inning;
+module.exports = half;
+module.exports = onFirst;
+module.exports = onSecond;
+module.exports = onThird;
+module.exports = totalRunners;
+
 
 // node baseballSimulator.js
 
