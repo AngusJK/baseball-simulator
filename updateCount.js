43b75ba99013a7bd57ae69987dadd1866df194aa
Angus Kellett <anguskellett@hotmail.com>
2020-12-10 22:54:22 +0000

    takes in a pitch type and updates the count accordingly

---
+++ updateCount.js
@@ -0,0 +1,23 @@
+const pitch = require("./pitch");
+
+let count = [0, 0];
+const updateCount = function(pitchType) {
+  if (pitchType === "Strike") {
+    count[1] += 1;
+  }
+  if (pitchType === "Ball") {
+    count[0] += 1;
+  }
+  if (count[0] === 4) {
+    count = [0, 0];
+    return "Ball four. Batter walks.";
+  }
+  if (count[1] === 3) {
+    count = [0, 0];
+    return "Strike three. Batter out.";
+
+  }
+  return count;
+}
+
+module.exports = updateCount;
