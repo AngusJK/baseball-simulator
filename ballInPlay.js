1d909070e0f54ed52f4300fbd36297c2f4301663
Angus Kellett <anguskellett@hotmail.com>
2020-12-11 20:34:20 +0000

    randomly generates result of ball hit in play

---
+++ ballInPlay.js
@@ -0,0 +1,18 @@
+const ballInPlay = function() {
+  let result = "";
+  let randNum = Math.floor(Math.random() * 2);
+  if (randNum < 0.65) {
+    result = "out";
+  } else if (randNum >= 0.65 && randNum < 0.85) {
+    result = "single";
+  } else if (randNum >= 0.85 && randNum < 0.94) {
+    result = "double";
+  } else if (randNum === 0.94) {
+    result = "triple";
+  } else {
+    result = "home run"; 
+  }
+  return result;
+}
+
+module.exports = ballInPlay;
