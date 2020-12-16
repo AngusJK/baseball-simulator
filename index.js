160319dd3f0825ed3f16afa5e48263bbc584b6c1
Angus Kellett <anguskellett@hotmail.com>
2020-12-16 01:05:21 +0000

    updates and announces current batter

--- index.js
+++ index.js
@@ -14,20 +14,34 @@ const rl = readline.createInterface({
 
 let count = [0, 0];
 let outs = 0;
+let batterIndex = 0;
+let currentBatter = visitingTeam[batterIndex];
+const updateBatter = function() {
+  batterIndex += 1;
+  if (batterIndex === 9) {
+    batterIndex = 0;
+  }
+  currentBatter = visitingTeam[batterIndex];
+  return currentBatter;
+}
+
 
 rl.question('Welcome to Major League Baseball Simulator 2020! Press "y" to play.\n', (answer1) => {
   if (answer1 === 'y') {
     console.log('Play ball!');
     ///////////////
+    console.log(`First batter up: ${currentBatter}.`);
     const throwPitch = function() {
       rl.question('Press "p" to throw a pitch.\n', (answer2) => {
+        let atBatOver = false;
         if (answer2 === 'p') {
           let inZone = pitch();
           if (inZone === false) {
             console.log("Ball");
             count[0] += 1;
             if (count[0] === 4) {
-              console.log("Ball 4. Batter walks.");
+              console.log(`Ball 4. ${currentBatter} walks.`);
+              atBatOver = true;
               count = [0, 0];
             } else {
               console.log(count);
@@ -36,7 +50,8 @@ rl.question('Welcome to Major League Baseball Simulator 2020! Press "y" to play.
             console.log("Strike");
             count[1] += 1;
             if (count[1] === 3) {
-              console.log("Strike 3. Batter out.");
+              console.log(`Strike 3. ${currentBatter} is out.`);
+              atBatOver = true;
               outs += 1;
               console.log(`${outs} out.`);
               count = [0, 0];
@@ -48,8 +63,10 @@ rl.question('Welcome to Major League Baseball Simulator 2020! Press "y" to play.
               console.log(count);
             }
           }
-
-
+          if (atBatOver === true) {
+            updateBatter();
+            console.log(`Now batting: ${currentBatter}.`);
+          }
           throwPitch();
         } else {
           console.log("You didn't press P. Bye.");
