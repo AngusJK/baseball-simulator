1660bb090213a49162c90930815e6dee8c42286f
Angus Kellett <anguskellett@hotmail.com>
2020-12-19 00:30:03 +0000

    adjusts RNG

--- ballInPlay.js
+++ ballInPlay.js
@@ -1,13 +1,13 @@
 const ballInPlay = function() {
   let result = "";
   let randNum = Math.random();
-  if (randNum < 0.65) {
+  if (randNum < 0.2) {
     result = "out";
-  } else if (randNum >= 0.65 && randNum < 0.85) {
+  } else if (randNum >= 0.2 && randNum < 0.5) {
     result = "single";
-  } else if (randNum >= 0.85 && randNum < 0.94) {
+  } else if (randNum >= 0.5 && randNum < 0.7) {
     result = "double";
-  } else if (randNum === 0.94) {
+  } else if (randNum >= 0.7 && randNum < 0.8) {
     result = "triple";
   } else {
     result = "home run"; 
