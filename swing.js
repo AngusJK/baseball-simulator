019b9b81d49552cb090766528cabd90082e40e62
Angus Kellett <anguskellett@hotmail.com>
2020-12-19 00:30:50 +0000

    adjusts RNG

--- swing.js
+++ swing.js
@@ -1,9 +1,9 @@
 const swing = function() {
   let result = '';
   let randNum = Math.random();
-  if (randNum < 0.3) {
+  if (randNum < 0.15) {
     result = "Foul";
-  } else if (randNum >= 0.3 && randNum < 0.6) {
+  } else if (randNum >= 0.15 && randNum < 0.2) {
     result = "Swing and miss";
   } else {
     result = "In play!";
